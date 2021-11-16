80e735bd421aafc6948796891616c8f8d21185c6
mathieu hollande <mathieuhollande@hotmail.fr>
2021-11-16 16:15:58 +0100

    success

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_16_111347) do
-
-  create_table "tasks", force: :cascade do |t|
-    t.string "title"
-    t.text "details"
-    t.boolean "completed", default: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
